fccca1a8b8f60094ba8b84b6b0ec9ae7f3ca6f44
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-05-31 14:05:34 +0530

    Chat engine integration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectId="57066153-7878-4220-b83c-8a28c943397f"
+            userName="Bhavya"
+            userSecret="123456789"
+        />
+    )
+}
+
+export default App;
